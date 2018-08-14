b051aa066c1662fe257688f0c8be4a10df08280c
JAYANTAROY1201 <roy.jayanta1201@gmail.com>
2018-08-14 12:49:24 +0530

    User Microservice

--- ToDoMicroserviceUserService/src/main/java/com/todo/controller/UserController.java
+++ ToDoMicroserviceUserService/src/main/java/com/todo/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -114,8 +115,8 @@ public class UserController {
 	 * @throws LoginException
 	 * @throws MessagingException
 	 */
-	@RequestMapping(value = "/forgetpassword", method = RequestMethod.POST)
-	public ResponseEntity<String> forgetPassword(@RequestParam String email) throws LoginException, MessagingException {
+	@RequestMapping(value = "/forgetpassword/{email}", method = RequestMethod.GET)
+	public ResponseEntity<String> forgetPassword(@PathVariable String  email) throws LoginException, MessagingException {
 
 		userService.doSendNewPasswordLink(email);
 
